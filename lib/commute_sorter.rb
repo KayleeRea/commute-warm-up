58ce9bfc9cd0a94850766c492ae9634845380047
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-07-07 14:58:24 -0600

    Data can be sorted by name, week and weekday

--- lib/commute_sorter.rb
+++ lib/commute_sorter.rb
@@ -1,10 +1,50 @@
 class CommuteSorter
-  def initialize(input)
-     @input = input
+  def group_by_name(input)
+    value = {}
+    input.each do |row|
+      name = row[0]
+      previous_trips = value.fetch(name) { [] }
+      trip = {
+        :week => row[1],
+        :day => row[2],
+        :mode => row[3],
+        :inbound => row[4],
+        :outbound => row[5],
+        :distance => row[6]
+      }
+      updated_trips = previous_trips << trip
+      value[name] = updated_trips.sort do |a, b|
+        same_week = compare_by_week(a, b).zero?
+        if same_week
+          compare_by_day(a, b)
+        else
+          compare_by_week(a, b)
+        end
+      end
+    end
+    value
   end
 
-  def by_name
-    split_data = @input.split(',')
-    { split_data.first => [{}] }
+  def compare_by_week(a, b)
+    a[:week] <=> b[:week]
+  end
+
+  def compare_by_day(a, b)
+    day_number(a[:day]) <=> day_number(b[:day])
+  end
+
+  def day_number(day)
+    case day
+      when 'Monday'
+        1
+      when 'Tuesday'
+        2
+      when 'Wednesday'
+        3
+      when 'Thursday'
+        4
+      when 'Friday'
+        5
+    end
   end
 end
--- spec/commute_sorter_spec.rb
+++ spec/commute_sorter_spec.rb
@@ -2,14 +2,102 @@ require 'spec_helper'
 require 'commute_sorter'
 
 describe CommuteSorter do
-  it 'sorts by name' do
-    input = <<INPUT.chomp
-Emily,4,Monday,Walk,12,15,0.65
-INPUT
+  it 'can group the csv rows by person' do
+    input = [
+      ["Nate", 1, "Tuesday", "Bus", 100, 110, 40],
+      ["Jenny", 4, "Tuesday", "Bike", 14, 11, 2.5],
+    ]
     expected =
       {
-        "Emily" => [{}]
-      };
-    expect(CommuteSorter.new(input).by_name).to eq expected
+        "Nate" => [
+          {
+            :week => 1,
+            :day => "Tuesday",
+            :mode => "Bus",
+            :inbound => 100,
+            :outbound => 110,
+            :distance => 40,
+          }
+        ],
+        "Jenny" => [
+          {
+            :week => 4,
+            :day => "Tuesday",
+            :mode => "Bike",
+            :inbound => 14,
+            :outbound => 11,
+            :distance => 2.5,
+          }
+        ],
+      }
+
+        commuter = CommuteSorter.new
+    expect(commuter.group_by_name(input)).to eq expected
+  end
+
+  it 'can sort the data by weekday and day' do
+    input = [
+      ["Elsa", 2, "Wednesday", "Drive", 30, 50, 24],
+      ["Elsa", 1, "Wednesday", "Bike", 10, 20, 14],
+      ["Elsa", 1, "Tuesday", "Bike", 10, 20, 14],
+      ["Elsa", 1, "Monday", "Bike", 10, 20, 14],
+      ["Elsa", 1, "Friday", "Bike", 10, 20, 14],
+      ["Elsa", 1, "Thursday", "Bike", 10, 20, 14],
+    ]
+
+    expected = {
+      "Elsa" => [
+        {
+          week: 1,
+          day: "Monday",
+          mode: "Bike",
+          inbound: 10,
+          outbound: 20,
+          distance: 14
+        },
+        {
+          week: 1,
+          day: "Tuesday",
+          mode: "Bike",
+          inbound: 10,
+          outbound: 20,
+          distance: 14
+        },
+        {
+          week: 1,
+          day: "Wednesday",
+          mode: "Bike",
+          inbound: 10,
+          outbound: 20,
+          distance: 14
+        },
+        {
+          week: 1,
+          day: "Thursday",
+          mode: "Bike",
+          inbound: 10,
+          outbound: 20,
+          distance: 14
+        },
+        {
+          week: 1,
+          day: "Friday",
+          mode: "Bike",
+          inbound: 10,
+          outbound: 20,
+          distance: 14
+        },
+        {
+          week: 2,
+          day: "Wednesday",
+          mode: "Drive",
+          inbound: 30,
+          outbound: 50,
+          distance: 24
+        },
+      ]
+    }
+    commuter = CommuteSorter.new
+    expect(commuter.group_by_name(input)).to eq expected
   end
 end
